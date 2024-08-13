d754a14c046795fc91d47fe94261b5deec97bb3e
Your Name <you@example.com>
2024-08-13 21:17:46 +0400

    updated

---
+++ mailer/send.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+
+require 'phpmailer/src/PHPMailer.php';
+
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+
+$mail = new PHPMailer (true);
+
+$mail->isSMTP();
+
+$mail->Host = 'smtp.gmail.com';
+
+$mail->SMTPAuth = true;
+
+$mail->Username = 'aswinstevin@gmail.com'; // Your gmail
+
+$mail->Password = 'zyni rxwm gjqv dhjk'; // Your gmail app password
+
+$mail->SMTPSecure = 'ssl';
+
+$mail->Port = 465;
+
+$mail->setFrom('aswinstevin@gmail.com'); // Your gmail
+
+$mail->addAddress($_POST["email"]);
+
+$mail->isHTML(true);
+
+$mail->Subject = $_POST["subject"];
+
+$mail->Body = $_POST["message"];
+
+$mail->send();
+
+header("location: index.html");
+
+}
+?>
+
+
+
+
